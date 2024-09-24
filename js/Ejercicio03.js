eaa16b0fcbe67a64559ff52d3c693c8ba15d70c0
Christian Paul Rodriguez Perez <230809@utxicotepec.edu.mx>
2024-09-24 12:50:43 -0600

    Practica 06
    Correccion de la sintaxis

--- js/Ejercicio03.js
+++ js/Ejercicio03.js
@@ -1,111 +1,116 @@
-const bg=`linear-gradient(11deg, rgba(2,0,36,1) 0%,rgba(9,9,121,1) 33%,rgba(0,212,255,1) 86%)`;
-const style_console = `backround ${bg}: color : white; border-radius: 6px; padding: 4px font-size: 1.0rem; front-weight:bold`;
- 
-// personalizacion de las salidas a cosnola 
-console.warn("--practica 05: repaso de objetos en java Script--");
+// Repaso de Objetos
 
-//variables independientes
-console.log("%c1.- variables independientes", style_console);
+const bg ="linear-grandient(11deg,rgba(2,0,36,1) 0%, rgba(9,9,121,1) 33%, rgba(0,212,255,1) 86%)";
+const style_console =`background: ${bg}; color: white; border-radius: 6px; padding: 4px; font-sixe: 1.0rem; font-weight: bold`
 
-// declaramos valores independientes relacionadas a un producto
+console.warn("Practixca 05: Repaso de Objetos en Java Script");
 
-let producto_nombre="computadora gamer laptop 17\"";
-let producto_marca="asus";
-let producto_modelo="tuf 17"
-let producto_precio=15749.50;
-let producto_disponibilidad=true;
-let producto_sku=Symbol("TUF707VV-HX221W")
-let producto_stock=15
-let producto_imagen=null
-let producto_barcode;
-let producto_categorias=["electronicos", "computacion","gaming","promocion buen fin","mejor valorados"];
+console.log("%c1.- Variables Independientes ",style_console);
 
-// accedemos a los valores de las caracteristicas del producto de manera independiente
+let producto_Nombre="Computadora  Gammer Laptop 17\"";
+let producto_Marca="Asus";
+let producto_Modelo="TUF 17";
+let Producto_precio=15749.50;
+let producto_Disponibilidad=true;
+let producto_SKU=Symbol("TUF707VV-HX221W")
+let producto_Stock=15;
+let producto_Imagen= null;
+let producto_Barcode;
+let producto_Categorias=["Electronicos","Computación","Gamming","Promociones Buen Fin","Mejor Valorados"];
 
-console.log(`los datos del producto son: \n
-    nombre: ${producto_nombre},
-    marca: ${producto_marca},
-    modelo: ${producto_modelo},
-    precio: ${producto_precio},
-    disponibilidad: ${producto_disponibilidad},
-    stock: ${producto_stock},
-    sku: ${producto_sku},
-    barcode: ${producto_barcode},
-    imagen: ${producto_imagen},
-    categorias:${producto_categorias}`);
 
-// ahora lo delcaramos como un objeto 
-let producto={
-    nombre : "tenis deportivos",
-    marca : "nike",
-    modelo : "jordan '24",
-    precio : "3360",
-    disponibilidad : false,
-    stock : 0,
-    sku : "DZ5485-512",
-    imagen : "../deportivo.snakrs./jordan.png",
-    barcode :null,
-    categorias:["deportivas","juvenil"]
-}
-// ahora leemos el objeto completo
+console.log(`Los datos del PRODUCTO son: \n 
+    Nombre: ${producto_Nombre}, Tipo de dato <${typeof(producto_Nombre)}>
+    Marca: ${producto_Marca}, Tipo de dato <${typeof(producto_Marca)}>
+    Modelo: ${producto_Modelo}, Tipo de dato <${typeof(producto_Modelo)}>
+    Precio: ${Producto_precio}, Tipo de dato <${typeof(Producto_precio)}>
+    Disponibilidad: ${producto_Disponibilidad}, Tipo de dato <${typeof(producto_Disponibilidad)}>
+    Stock: ${producto_Stock}, Tipo de dato <${typeof(producto_Stock)}>
+    Barcode: ${producto_Barcode}, Tipo de dato <${typeof(producto_Barcode)}>
+    Imagen: ${producto_Imagen}, Tipo de dato <${typeof(producto_Imagen)}>
+    Categorias: ${producto_Categorias}, Tipo de dato <${typeof(producto_Categorias)}>`);
 
-console.table(producto)
 
-//Para acceder a las propiedades del objeto utilizamos un "."  espacio y el nombre de la propiedad a leer 
-console.log("Accediendo a propiedades especificas del producto ");
-console.log(`Nombre completo del Producto :  ${producto.nombre}, ${producto.marca} , ${producto.modelo},`);
-console.log(`Precio : ${precio}`);
+    console.log("En el caso del SKU al ser un Simbol, no se puede concatenar a la cadena de impresión anterior")
+    console.log(producto_SKU);
+    console.log(typeof(producto_SKU));
 
-if(producto.disponibilidad==0){
-console.log("Estatus agotado");
-}else{
-console.log(`Estatus : ${producto_stock} Unidades disponibles `);
-
-}
-
-console.log("%c3. Destructuración de objetos",style_console);
-
-let producto2={
-    clave:3216,
-    nombre : "Lentes para sol",
-    marca : "Oaklet",
-    modelo : "Qntm kato",
-    precio : "6829.0",
-    disponibilidad : true,
-    stock : 5,
-    sku : "OO9481D_0356",
-    imagen : "../deportivo.snakrs./Qtned.png",
-    barcode :888392491626,
-    categorias:["deportivas","Lentes","Hombre","Accesorios"]
-}
-    
-
-let Comprador ={
-    clave: 3216,
-    nombre : "Christian",
-     apellido:"Rodriguez",
-     tipo: "frecuente",
-     correo: "Rodriguez@gmail.com",
-     paisorigen:"Mexico"
-}
-
-let pedido={
-  producto_clave:3216,
-  Comprador_clave:3216,
-  cantidad: 2,
-  status:"Carrito de comptra"
-}
-
-//En base a los tres objetos necesitamos calcular el costo de la compra y si le alcanza su saldo
-    let[producto_precio: precio] = producto2;
-    let[producto_cantidad: cantidad ] =pedido;
-    let[cliente_saldoactual: saldoactual]= Comprador;
-
-    console.log(`El cliente a agregado su carrito ${pedido_cantidad} unidades, con un costo total de : ${(pedido_cantidad*producto_precio)}`)
-    
-  if(costo_compra>actual)
+// ahora lo declaramos como un objeto
+console.log("%c2.- Variables Independientes ",style_console);
+    let producto= 
+    {
+        Nombre: "Tenis Deportivos",
+        Marca: "Nke",
+        Modelo: "Jodan ' 24",
+        Precio: 3361.25,
+        Disponiblidad: false,
+        Stock: 0,
+        SKU: "DZ5485-612",
+        Imagen: "../Assets/Products/sneaker/JORDAN.png",
+        Barcode: null,
+        Categorias:["Deportes", "Tenis","Juvenil"]
 
     
+    }
+
+    // Ahora leemos el objeto 
+    console.table(producto)
+
+    // Para acceder a las propiedades del objeto utilizamos un "." y el nombre de la propiedad a leer.
+
+    console.log("Accediendo a propiedades especificas del PRODUCTO")
+    console.log(`Nombre Completo del PRODUCTO: ${producto.Nombre} ${producto.Marca} ${producto.Modelo} `)
+    console.log(`Precio: ${producto.Precio}`)
+    if(producto.Disponiblidad ==0)
+    console.log(`Estatus: Agotado`)
+    else
+    console.log(`Estatus: ${producto_Stock} , Unidades disponibles`)
+
+
+    console.log("%c3.- Destructuración de Objetos ",style_console);
+    let producto2= 
+    {
+        Clave: 316,
+        Nombre: "Lentes para sol",
+        Marca: "Oakley",
+        Modelo: "QNTM Kato",
+        Precio: 6829.00,
+        Disponiblidad: true,
+        Stock: 5,
+        SKU: "OO9481D-03",
+        Imagen: "../Assets/Products/sunglasses/katoRed.png",
+        Barcode: 888392491626,
+        Categorias:["Deportes", "Lentes","Accesorios"]
+
     
+    }
+
+    let Comprador=
+    {
+        Clave:3216,
+        Nombre: "Karen",
+        Apellidos: "Negrete Hernández",
+        Tipo: "Frecuente",
+        Correo: "karen@gmail.com",
+        PaisOrigen: "México",
+        SaldoActual: 14000.00
+    }
+    let Pedido=
+    {
+        Producto_Clave: 316,
+        Comprador_Clave: 3216,
+        Cantidad: 2,
+        Estatus: "Carrito de compra"
+    }
+
+    // 
+
+    let { Precio: producto_precio2}=producto2;
+    let {Cantidad:pedido_cantidad}=Pedido;
+    let {SaldoActual:Cliente_SaldoActual}=Comprador;
+    let Costo_compra=Producto_precio*pedido_cantidad;
+
     
-    
+    console.log(`El cliente ha agregado a su carrito de compras ${pedido_cantidad} unidades, con un costo total de: ${Costo_compra}`)
+    if(Costo_compra<Cliente_SaldoActual)
+        console.log("El cliente tiene saldo suficiente");
